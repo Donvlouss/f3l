eccf4c5ebf63e8d8ec185d236d109d2f13b8177f
Donvlouss <donvlous0rixk@gmail.com>
2024-02-29 09:33:01 +0800

    add statistical outlier removal

---
+++ crates/f3l_filter/examples/statistical_outlier_removal.rs
@@ -0,0 +1,42 @@
+#[cfg(feature = "app_kiss3d")]
+use kiss3d::light::Light;
+#[cfg(feature = "app_kiss3d")]
+use kiss3d::window::Window;
+#[cfg(feature = "app_kiss3d")]
+use nalgebra::Point3;
+
+mod util;
+use util::load_ply;
+
+use f3l_filter::*;
+
+#[cfg(not(feature = "app_kiss3d"))]
+fn main() {
+    println!("Add --features=app_kiss3d")
+}
+
+#[cfg(feature = "app_kiss3d")]
+fn main() {
+    println!("Using Kiss3d app");
+
+    let mut window = Window::new("Kiss3d: points");
+
+    window.set_light(Light::StickToCamera);
+    window.set_point_size(10.0); // (Not supported by all graphic drivers)
+
+    let vertices = load_ply("data/Itable_scene_lms400.ply");
+    let mut filter = StatisticalOutlierRemoval::with_data(1., 50, &vertices);
+    filter.set_negative(true);
+    let out = filter.filter_instance();
+
+    while window.render() {
+        vertices.iter()
+            .for_each(|v| {
+                window.draw_point(&Point3::new(v[0], v[1], v[2]), &Point3::new(1.0, 1.0, 1.0));
+            });
+        out.iter()
+            .for_each(|v| {
+                window.draw_point(&Point3::new(v[0], v[1], v[2]), &Point3::new(0.0, 1.0, 0.0));
+            });
+    }
+}
--- crates/f3l_filter/src/lib.rs
+++ crates/f3l_filter/src/lib.rs
@@ -2,6 +2,7 @@ mod radius_outlier_removal;
 mod voxel_grid;
 mod pass_through;
 mod condition_removal;
+mod statistical_outlier_removal;
 
 pub trait F3lFilter<'a, P>
 {
@@ -17,3 +18,4 @@ pub use radius_outlier_removal::RadiusOutlierRemoval;
 pub use voxel_grid::VoxelGrid;
 pub use pass_through::PassThrough;
 pub use condition_removal::ConditionRemoval;
+pub use statistical_outlier_removal::StatisticalOutlierRemoval;
