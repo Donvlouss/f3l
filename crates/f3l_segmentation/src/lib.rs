01bebc5ede8731f10adb13561ae9f853e68f030c
Donvlouss <donvlous0rixk@gmail.com>
2024-03-01 09:37:20 +0800

    Add euclidean cluster extraction

--- crates/f3l_segmentation/src/lib.rs
+++ crates/f3l_segmentation/src/lib.rs
@@ -1,14 +1,39 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use f3l_core::BasicFloat;
+
+mod euclidean_cluster_extraction;
+
+#[derive(Debug, Clone, Copy, Default)]
+pub struct F3lClusterParameter<T: BasicFloat> {
+    pub tolerance: T,
+    pub nb_in_tolerance: usize,
+    pub min_nb_data: usize,
+    pub max_nb_data: usize,
+    pub max_nb_cluster: usize
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl<T: BasicFloat> F3lClusterParameter<T> {
+    pub fn set_min_data_in_cluster(&mut self, nb: usize) {
+        self.min_nb_data = nb;
+    }
+    pub fn set_max_data_in_cluster(&mut self, nb: usize) {
+        self.max_nb_data = nb;
+    }
+    pub fn set_max_clusters(&mut self, nb: usize) {
+        self.max_nb_cluster = nb;
     }
 }
+
+pub trait F3lCluster<'a, T: BasicFloat, P> {
+    fn set_parameter(&mut self, parameter: F3lClusterParameter<T>);
+    fn parameter(&self) -> F3lClusterParameter<T>;
+
+    fn set_data(&mut self, data: &'a Vec<P>);
+    fn clusters(&self) -> usize;
+    /// vector of points of cluster nb
+    fn extract(&mut self) -> Vec<Vec<usize>>;
+    fn apply_extract(&mut self) -> bool;
+    fn at(&self, id: usize) -> Result<Vec<P>, String>;
+    fn max_cluster(&self) -> Vec<P>;
+}
+
+pub use euclidean_cluster_extraction::EuclideanClusterExtractor;
